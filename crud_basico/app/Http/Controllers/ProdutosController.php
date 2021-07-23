d028d716293d38695d68b0709acee07cb39976a2
gabriel.jesus <gabriel.jesus@app4point.com.br>
2021-07-23 15:25:10 -0300

    Create a new Laravel Project/Create MVC structure for Products

---
+++ crud_basico/app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    public function create()
+    {
+        return view('produtos.create');
+    }
+}
---
+++ crud_basico/resources/views/produtos/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre um novo produto</title>
+</head>
+
+<body>
+    <form action="">
+        <label for="">Nome</label> <br>
+        <input type="text" name="nome"> <br>
+        <label for="">Custo</label> <br>
+        <input type="text" name="custo"> <br>
+        <label for="">Preco</label> <br>
+        <input type="text" name="preco"> <br>
+        <label for="">Quantidade</label> <br>
+        <input type="text" name="quantidade"> <br>
+        <button>Salvar</button>
+    </form>
+</body>
+
+</html>
